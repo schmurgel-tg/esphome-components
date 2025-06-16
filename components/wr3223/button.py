99f4ab8a442b49dfc27f6613282fab7ed28b72d0
schmurgel-tg <marcus.kublank@gmx.de>
2025-06-16 16:41:50 +0200

    added default name for update relais button

--- components/wr3223/button.py
+++ components/wr3223/button.py
@@ -1,9 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import button
-from esphome.const import ENTITY_CATEGORY_CONFIG, CONF_ID
+from esphome.const import ENTITY_CATEGORY_CONFIG, CONF_ID, CONF_NAME
 
-from . import wr3223_ns, CONF_WR3223_RELAIS_COMPONENT_ID
+from . import wr3223_ns, CONF_WR3223_RELAIS_COMPONENT_ID, CONF_DEACTIVATE
 from .binary_sensor import WR3223RelaisComponent
 
 WR3223RelaisUpdateButton = wr3223_ns.class_(
@@ -17,10 +17,10 @@ CONFIG_SCHEMA = (
         entity_category=ENTITY_CATEGORY_CONFIG,
     )
     .extend(
-        {
-            cv.GenerateID(CONF_WR3223_RELAIS_COMPONENT_ID): cv.use_id(
-                WR3223RelaisComponent
-            )
+        {            
+            cv.GenerateID(CONF_WR3223_RELAIS_COMPONENT_ID): cv.use_id(WR3223RelaisComponent),
+            cv.Optional(CONF_NAME, default="Update Relais"): cv.string_strict,
+            cv.Optional(CONF_DEACTIVATE, default=False): cv.boolean,
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
@@ -29,6 +29,8 @@ CONFIG_SCHEMA = (
 
 async def to_code(config):
     relais = await cg.get_variable(config[CONF_WR3223_RELAIS_COMPONENT_ID])
-    var = cg.new_Pvariable(config[CONF_ID], relais)
-    await cg.register_component(var, config)
-    await button.register_button(var, config)
+    conf = config[CONF_ID]
+    if not conf.get(CONF_DEACTIVATE):
+        var = cg.new_Pvariable(conf, relais)
+        await cg.register_component(var, config)
+        await button.register_button(var, config)
