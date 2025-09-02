f371a6b9f658f5e6e0c4ac469c6498c2bf1caaef
schmurgel-tg <marcus.kublank@gmx.de>
2025-09-02 16:46:26 +0200

    default values dirket im schema gesetzt

--- components/wr3223/sensor.py
+++ components/wr3223/sensor.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor as sensor_comp
+from esphome.components import sensor
 from esphome.const import (
     UNIT_CELSIUS,
     DEVICE_CLASS_TEMPERATURE,
@@ -9,7 +9,7 @@ from esphome.const import (
     CONF_DEVICE_CLASS,
     CONF_NAME,
     CONF_SENSORS,
-    CONF_ID,    
+    CONF_STATE_CLASS,
     CONF_UPDATE_INTERVAL,
     CONF_UNIT_OF_MEASUREMENT,
     CONF_ACCURACY_DECIMALS,
@@ -53,57 +53,59 @@ def validate_custom_command(value):
 
 
 # **Definition der einzelnen Temperatur-Sensoren**
-CONFIG_SCHEMA = (
-    sensor_comp.sensor_schema()
-    .extend(
-        {
-            # Default Id setzen, damit YAML nichts angeben muss:
-            cv.Optional(CONF_ID, default="wr3223_sensor"): cv.declare_id(sensor_comp.Sensor),
-
-            cv.GenerateID(CONF_WR3223_ID): cv.use_id(WR3223),
-
-            # **Standard-Sensoren mit IntelliSense (NUR vordefinierte Werte)**
-            cv.Optional(CONF_SENSORS, default={}): cv.Schema(
-                {
-                    cv.Optional(k, default={}): cv.Schema(
-                        {
-                            cv.GenerateID(
-                                CONF_SENSOR_POLLING_COMPONENT_ID
-                            ): cv.declare_id(WR3223SensorPollingComponent),
-                            cv.Optional(
-                                CONF_DEACTIVATE, default=False
-                            ): cv.boolean,  # Sensor deaktivieren
-                        }
-                    )
-                    .extend(sensor_comp.SENSOR_SCHEMA)
-                    .extend(cv.polling_component_schema("60s"))
-                    for k in SENSOR_COMMANDS.keys()
-                }
-            ),
-            # **Benutzerdefinierte Sensoren (MÜSSEN genau 2 Zeichen haben + Pflichtfelder)**
-            cv.Optional(CONF_SENSORS_CUSTOM, default=[]): cv.ensure_list(
-                sensor_comp.sensor_schema(state_class=STATE_CLASS_MEASUREMENT)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_WR3223_ID): cv.use_id(WR3223),
+        # **Standard-Sensoren mit IntelliSense (NUR vordefinierte Werte)**
+        cv.Optional(CONF_SENSORS, default={}): cv.Schema(
+            {
+                cv.Optional(k, default={}): sensor.sensor_schema()
                 .extend(
                     {
                         cv.GenerateID(CONF_SENSOR_POLLING_COMPONENT_ID): cv.declare_id(
                             WR3223SensorPollingComponent
                         ),
-                        cv.Required(CONF_COMMAND): cv.All(
-                            cv.string, validate_custom_command
-                        ),
-                        cv.Required(CONF_NAME): cv._validate_entity_name,
                         cv.Optional(
-                            CONF_UNIT_OF_MEASUREMENT
-                        ): sensor_comp.validate_unit_of_measurement,
-                        cv.Optional(CONF_DEVICE_CLASS): sensor_comp.validate_device_class,
+                            CONF_DEACTIVATE, default=False
+                        ): cv.boolean,  # Sensor deaktivieren
+                        cv.Optional(
+                            CONF_NAME, default=SENSOR_COMMANDS[k][0]
+                        ): cv._validate_entity_name,
+                        cv.Optional(
+                            CONF_UNIT_OF_MEASUREMENT, default=SENSOR_COMMANDS[k][1]
+                        ): sensor.validate_unit_of_measurement,
+                        cv.Optional(
+                            CONF_DEVICE_CLASS, default=SENSOR_COMMANDS[k][2]
+                        ): sensor.validate_device_class,
                     }
                 )
-                .extend(cv.polling_component_schema("60s")),
-            ),
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
-)
+                .extend(sensor.SENSOR_SCHEMA)
+                .extend(cv.polling_component_schema("60s"))
+                for k in SENSOR_COMMANDS.keys()
+            }
+        ),
+        # **Benutzerdefinierte Sensoren (MÜSSEN genau 2 Zeichen haben + Pflichtfelder)**
+        cv.Optional(CONF_SENSORS_CUSTOM, default=[]): cv.ensure_list(
+            sensor.sensor_schema(state_class=STATE_CLASS_MEASUREMENT)
+            .extend(
+                {
+                    cv.GenerateID(CONF_SENSOR_POLLING_COMPONENT_ID): cv.declare_id(
+                        WR3223SensorPollingComponent
+                    ),
+                    cv.Required(CONF_COMMAND): cv.All(
+                        cv.string, validate_custom_command
+                    ),
+                    cv.Required(CONF_NAME): cv._validate_entity_name,
+                    cv.Optional(
+                        CONF_UNIT_OF_MEASUREMENT
+                    ): sensor.validate_unit_of_measurement,
+                    cv.Optional(CONF_DEVICE_CLASS): sensor.validate_device_class,
+                }
+            )
+            .extend(cv.polling_component_schema("60s")),
+        ),
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
 
 async def generate_sensor_code(parent, sensor_config):
@@ -111,7 +113,7 @@ async def generate_sensor_code(parent, sensor_config):
 
     command = sensor_config[CONF_COMMAND]
 
-    sens = await sensor_comp.new_sensor(sensor_config)
+    sens = await sensor.new_sensor(sensor_config)
 
     var = cg.new_Pvariable(
         sensor_config[CONF_SENSOR_POLLING_COMPONENT_ID],
@@ -136,12 +138,6 @@ async def to_code(config):
             continue  # Sensor nicht erstellen, wenn deaktiviert
 
         sensor_config[CONF_COMMAND] = command
-        # Standardwerte setzen, falls nicht vorhanden
-        sensor_config.setdefault(
-            CONF_NAME, SENSOR_COMMANDS[command][0]
-        )  # Name aus `SENSOR_COMMANDS`
-        sensor_config.setdefault(CONF_UNIT_OF_MEASUREMENT, SENSOR_COMMANDS[command][1])
-        sensor_config.setdefault(CONF_DEVICE_CLASS, SENSOR_COMMANDS[command][2])
         sensor_config.setdefault(CONF_ACCURACY_DECIMALS, 1)
 
         await generate_sensor_code(parent, sensor_config)
